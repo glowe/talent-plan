310e4cd9e05a06df04cff2b1f5dc52236ef4de36
Graham Lowe <glowe@users.noreply.github.com>
2022-12-30 21:15:44 -0500

    Add multi-threading to server

--- courses/rust/projects/project-4/src/server.rs
+++ courses/rust/projects/project-4/src/server.rs
@@ -2,6 +2,8 @@ use crate::engines::KvsEngine;
 use crate::error::Result;
 use crate::protocol::Request;
 use crate::protocol::Response;
+use crate::thread_pool::NaiveThreadPool;
+use crate::thread_pool::ThreadPool;
 use rmp_serde::Deserializer;
 use rmp_serde::Serializer;
 use serde::Deserialize;
@@ -25,44 +27,50 @@ impl<E: KvsEngine> KvsServer<E> {
         Self { engine, log }
     }
 
-    fn read_request(&mut self, stream: &mut TcpStream) -> Result<Request> {
-        let mut reader = Deserializer::new(BufReader::new(stream));
-        Ok(Request::deserialize(&mut reader)?)
-    }
-
-    fn process_request(&mut self, request: Request) -> Response {
-        match request {
-            Request::Get(key) => match self.engine.get(key.clone()) {
-                Ok(value) => Response::GetOk(value.clone()),
-                Err(err) => Response::Err(err.to_string()),
-            },
-            Request::Set(key, value) => match self.engine.set(key, value) {
-                Ok(()) => Response::SetOk(()),
-                Err(err) => Response::Err(err.to_string()),
-            },
-            Request::Remove(key) => match self.engine.remove(key) {
-                Ok(()) => Response::RemoveOk(()),
-                Err(err) => Response::Err(err.to_string()),
-            },
-        }
-    }
-
-    fn respond(&mut self, stream: TcpStream, response: &mut Response) -> Result<()> {
-        let mut writer = Serializer::new(BufWriter::new(&stream));
-        response.serialize(&mut writer)?;
-        writer.get_mut().flush()?;
-        Ok(())
-    }
-
     pub fn serve(&mut self, addr: &SocketAddr) -> Result<()> {
         let listener = TcpListener::bind(addr)?;
+        let thread_pool = NaiveThreadPool::new(32)?;
         for result in listener.incoming() {
-            debug!(self.log, "received connection");
             let mut stream = result?;
-            let request = self.read_request(&mut stream)?;
-            let mut response = self.process_request(request);
-            self.respond(stream, &mut response)?;
+            let engine = self.engine.clone();
+            let log = self.log.clone();
+            thread_pool.spawn(move || {
+                let request = read_request(&mut stream).unwrap();
+                debug!(&log, "request = {:?}", request);
+                let mut response = process_request(&engine, request);
+                debug!(&log, "response = {:?}", response);
+                respond(stream, &mut response).unwrap();
+            });
         }
         Ok(())
     }
+}
+
+fn read_request(stream: &mut TcpStream) -> Result<Request> {
+    let mut reader = Deserializer::new(BufReader::new(stream));
+    Ok(Request::deserialize(&mut reader)?)
+}
+
+fn process_request<E: KvsEngine>(engine: &E, request: Request) -> Response {
+    match request {
+        Request::Get(key) => match engine.get(key.clone()) {
+            Ok(value) => Response::GetOk(value.clone()),
+            Err(err) => Response::Err(err.to_string()),
+        },
+        Request::Set(key, value) => match engine.set(key, value) {
+            Ok(()) => Response::SetOk(()),
+            Err(err) => Response::Err(err.to_string()),
+        },
+        Request::Remove(key) => match engine.remove(key) {
+            Ok(()) => Response::RemoveOk(()),
+            Err(err) => Response::Err(err.to_string()),
+        },
+    }
+}
+
+fn respond(stream: TcpStream, response: &mut Response) -> Result<()> {
+    let mut writer = Serializer::new(BufWriter::new(&stream));
+    response.serialize(&mut writer)?;
+    writer.get_mut().flush()?;
+    Ok(())
 }
